288da10a179cbec0691a68480d524fc66e961bbd
sandraldr27 <sandralilder@gmail.com>
2023-12-16 22:58:39 +0100

    Components and service are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListaEmpleadosComponent } from './components/lista-empleados/lista-empleados.component';
+import { EmpleadoComponent } from './components/empleado/empleado.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListaEmpleadosComponent,
+    EmpleadoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
